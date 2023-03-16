ce99086c637085605f423e162a681ae4917088f1
Felix Sindani <felixsindani72@gmail.com>
2023-03-16 14:22:46 -0700

    Modified 4-puts source file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  * Description : Printing "programming is like building a multilingual puzzle"
  * Return : Always 0 (Success)
  */
-int main(void)
+main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle\");
 	return (0);
 }
