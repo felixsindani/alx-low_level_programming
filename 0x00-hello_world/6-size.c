9788943d5ed764575fb660e0f6d90f24561db5be
Felix Sindani <felixsindani72@gmail.com>
2023-03-16 17:07:02 -0700

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(Size of a char : % lu byte(s) \n", size of (char));
+	printf(Size of a an int: % lu byte(s) \n", size of (int));
+	printf(Size of a long : % lu bytes(s) \n", size of (long));
+	printf(Size of a long long int: % lu byte(s) \n", size of (d));
+	printf(size of a float : % i byte(s) \n", size of (%f));
+	return (0);
+}
