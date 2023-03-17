cb237fae032e91317e8b41c82c706d44c30dfeed
Felix Sindani <felixsindani72@gmail.com>
2023-03-17 11:45:25 -0700

    Fixed comments format and indentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<stdio.h>
-
-/*
- * main - entry point
- * program display numbers and  signs
- * betty style doc for function main goes there
+/**
+ * Main - Entry point
+ * Program to generate random numbers
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -17,15 +15,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	printf("%d is positive", n);
+	printf("%d is positive ", n);
 	}
 	else if (n < 0)
 	{
-	printf("%d is negative", n);
+	printf("%d is negative ", n);
 	}
 	else
 	{
-	printf("%d is zero", n);
+	printf("%d is zero ", n);
 	}
 	return (0);
 }
