9c8a73f6e58e9fab50af3a8048c85babccecddfc
Felix Sindani <felixsindani72@gmail.com>
2023-03-17 13:12:16 -0700

    Uploaded  programthat  assign a random number to the variable n each time it is executed

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints lowercase alphabets
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
