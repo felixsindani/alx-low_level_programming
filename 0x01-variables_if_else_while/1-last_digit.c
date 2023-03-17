88ef4caf23471b47726cbc42b1f68b00f0d148a5
Felix Sindani <felixsindani72@gmail.com>
2023-03-17 15:48:18 -0700

    Fixed next line issue on program that assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,17 +20,16 @@ int main(void)
 	printf("Last digit of %d is %d", n, b);
 	if (b > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	if (b == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
 	if (b < 6 && b != 0)
 	{
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
-	printf("\n");
 
 	return (0);
 }
