332e9bc31c9215b7945448e762c7d436dad21687
Felix Sindani <felixsindani72@gmail.com>
2023-03-18 12:36:40 -0700

    Modified size of data types program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf(Size of a char : % lu byte(s)\n",(unsigned long)size of (char));
-	printf(Size of a an int : % lu byte(s) \n",(unsigned long)size of (int));
-	printf(Size of a long : % lu bytes(s) \n",(unsigned long)size of (long));
-	printf(Size of a long long int : % lu byte(s) \n",(unsigned long)size of (d));
-	printf(size of a float : % i byte(s) \n",(unsigned long)size of (%f));
+	printf(Size of a char : % lu byte(s)\n", (unsigned long)sizeof (char));
+	printf(Size of a an int : % lu byte(s) \n", (unsigned long)sizeof(int));
+	printf(Size of a long : % lu bytes(s) \n", (unsigned long)sizeof (long));
+	printf(Size of a long long int : % lu byte(s) \n", (unsigned long)sizeof(d));
+	printf(size of a float : % i byte(s) \n", (unsigned long)sizeof (%f));
 	return (0);
 }
