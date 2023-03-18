1d3c0abbae9e0294350f54297a0c29860c9263e4
Felix Sindani <felixsindani72@gmail.com>
2023-03-18 04:12:22 -0700

    modified 6-size file synta

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf(Size of a char : % lu byte(s) \n", size of (char));
-	printf(Size of a an int: % lu byte(s) \n", size of (int));
-	printf(Size of a long : % lu bytes(s) \n", size of (long));
-	printf(Size of a long long int: % lu byte(s) \n", size of (d));
-	printf(size of a float : % i byte(s) \n", size of (%f));
+	printf(Size of a char: %lu byte(s)\n", (unsigned long)size of (char));;
+	printf(Size of a an int: % lu byte(s) \n", (unsigned long)size of (int));
+	printf(Size of a long : % lu bytes(s) \n", ((unsigned long)size of (long));
+	printf(Size of a long long int: % lu byte(s) \n", (unsigned long)size of (d));
+	printf(size of a float : % i byte(s) \n", (unsigned long)size of (%f));
 	return (0);
 }
