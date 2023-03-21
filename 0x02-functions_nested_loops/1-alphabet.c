98ca34fd3614ea123d42e9568d09aecb6f621d53
Felix Sindani <felixsindani72@gmail.com>
2023-03-21 09:33:22 -0700

    Created program that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * print_alphabet - function
+ * prints the alphabet,in lowercase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a', alphabets <= 'z'; alphabets++)
+		_putchar(alphabets);
+	_putchar('\n');
+}
