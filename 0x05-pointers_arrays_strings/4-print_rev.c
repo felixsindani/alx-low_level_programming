35704a8c9a7d8f22f1f706c4f79f3d4d930c13e6
Felix Sindani <felixsindani72@gmail.com>
2023-03-27 17:29:25 -0700

    Added unction that prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse
+ * print_rev - prints a string in reverse
  * @s: string
- * Return: 0
  */
 void print_rev(char *s)
 {
-	int long i = 0;
-	int o;
+	int a, b;
 
-	while (*s != '\0')
+	b = 0;
+	while (s[b] != '\0')
+		b++;
+
+	for (a = b - 1; a >= 0; a--)
 	{
-		long i++;
-		s++;
-	}
-	s--;
-	for (o = long i; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 }
