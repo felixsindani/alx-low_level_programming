25c42ebf391bc0b3ead80d757a84e076b08fec5a
Felix Sindani <felixsindani72@gmail.com>
2023-04-05 14:22:56 -0700

    created function that returns the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a number
+ * @n: random number
+ * Return: n
+ */
+int factorial(int n)
+{
+	int p;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	{
+		p = n * factorial(n - 1);
+		return (j);
+	}
+}
