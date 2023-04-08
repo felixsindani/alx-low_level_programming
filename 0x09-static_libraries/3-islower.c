a716d8f8c88bb6a0c64890874145fda18883f3d6
Felix Sindani <felixsindani72@gmail.com>
2023-04-08 12:57:59 -0700

    Created 3-islower.c file

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - prints lower case
+ * @c: string to be printed
+ * Return: 1 lowercase
+ */
+int _slower(int c)
+{
+	if(c > 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
