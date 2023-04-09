228594295868a5a9f15269c6bbf79017f9da4bd3
Felix Sindani <felixsindani72@gmail.com>
2023-04-09 12:34:08 -0700

    fixed bug on mynewnameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: - vector
  * Return: 0
  */
-int main(int argc __attribute__ ((unused)), char __attribute__ ((unused)) *argv[])
+int main(int argc __attribute__((unused)), char __attribute__((unused))*argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
