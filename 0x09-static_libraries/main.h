7dddb7e74f22f79cb9139cfa3b67f6d22c941c7d
Felix Sindani <felixsindani72@gmail.com>
2023-04-09 08:29:34 -0700

    Modified main.h include guard

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN.H
+#define MAIN.H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int main(void);
+#endif
