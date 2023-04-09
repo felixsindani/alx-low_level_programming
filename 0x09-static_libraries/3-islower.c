c6314681f660cf491b6daaf76897c769b7e0dadd
Felix Sindani <felixsindani72@gmail.com>
2023-04-09 08:50:31 -0700

    Fix output bug

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -5,9 +5,9 @@
  * @c: string to be printed
  * Return: 1 lowercase
  */
-int _slower(int c)
+int _islower(int c)
 {
-	if(c > 97 && c <= 122)
+	if (c > 97 && c <= 122)
 		return (1);
 	else
 		return (0);
