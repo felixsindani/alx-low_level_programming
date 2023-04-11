5fa40ea22587b2476981257e1345ee91847a5fce
Felix Sindani <felixsindani72@gmail.com>
2023-04-10 19:10:59 -0700

    Modified function to add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,54 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * check_num 
+ * check_num - check for digits
+ * @str: string
+ * Return: 0
+ */
+int check_num(char *str)
+{
+	unsigned int counter;
+
+	counter = 0;
+	while (counter < strlen(*str[counter]))
+	{
+		if (!isdigit(*str[counter]))
+		{
+			return (0);
+		}
+		counter++;
+	}
+	return (1);
+}
+/**
+ * main - prints name
+ * @argc: counts arguments
+ * @argv: Arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int counter;
+	int str_to_int;
+	int sum = 0;
+
+	counter = 1;
+	while (counter < argc)
+	{
+		if (check_num(argv[counter]))
+		{
+			str_to_int = atoi(argv[counter]);
+			sum += str_to_int;
+		}
+		else
+		{
+		printf("error\n");
+		return (1);
+		}
+		counter++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,4 +4,5 @@ int main(argc, char *argv[]);
 int _putchar(char c);
 int _atoi(char *s);
 int main();
+int check_num(char *str);
 #endif
