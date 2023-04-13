895101326abe9a71c750c204f92ae9a66c865ed0
Felix Sindani <felixsindani72@gmail.com>
2023-04-13 14:06:40 -0700

    fix bug on function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
+	unsigned int val;
 	
 	array = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
                 return (NULL);
 	if (array == NULL)
 		return (NULL);
+	for (val = 0; val < (nmemb * size); val++)
+		array[val] = 0;
 	return (array);
 }
