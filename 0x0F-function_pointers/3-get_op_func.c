71e62c4c124612dd039b4d46dfd751e223f926e4
Felix Sindani <felixsindani72@gmail.com>
2023-04-19 15:59:15 -0700

    Updated 3-get_op_func.c file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that picks operator func
+ * @s: operator passed as argument
+ * Return: pointer to operator function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-"; op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int index = 0;
+
+	while (ops[index].op != NULL && *(ops[index].op) != *s)
+		index++;
+	return (ops[index].f);
+}
