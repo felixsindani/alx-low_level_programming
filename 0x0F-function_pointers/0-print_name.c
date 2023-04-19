9b77bab484b52698088a8636bad6f3a562e59508
Felix Sindani <felixsindani72@gmail.com>
2023-04-19 12:38:37 -0700

    Fix betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,7 +9,7 @@ void print_name(char *name, void (*f)(char *));
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return ;
+	while (name == NULL || f == NULL)
+		return;
 	f(name);
 }
