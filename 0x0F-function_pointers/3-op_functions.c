509110f6316f83ef615b1337653b241ef104badb
Felix Sindani <felixsindani72@gmail.com>
2023-04-19 15:27:20 -0700

    Defined functions in 3-op_function.c file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "calc.h"
+
+/**
+ * op_add - function that add
+ * @a: num1
+ * @b: num2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	int a, b;
+
+	return (a + b);
+}
+
+/**
+ * op_sub - function that subtract numbers
+ * @a: num1
+ * @b: num2
+ * Return: Difference
+ */
+int op_sub(int a, int b)
+{
+	int a, b;
+
+	return (a - b);
+}
+/**
+ * op_mul - product function
+ * @a: num1
+ * @b: num2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	int a, b;
+
+	return (a * b);
+}
+
+/**
+ * op_div - function dividing numbers
+ * @a: num1
+ * @b: num2
+ * Return: Result of division
+ */
+int op_div(int a, int b)
+{
+	int a, b;
+
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo function
+ * @a: num1
+ * @b: num2
+ * Return: remainder of division
+ */
+int op_mod(int a, int b)
+{
+	int a, b;
+
+	return (a % b);
+}
