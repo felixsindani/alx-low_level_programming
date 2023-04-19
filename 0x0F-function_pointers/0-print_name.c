c2c40690971fc6d537bead9067c489475ae3a00d
Felix Sindani <felixsindani72@gmail.com>
2023-04-19 12:32:53 -0700

    unction that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+void print_name(char *name, void (*f)(char *));
+/**
+ * print_name - function that prints name
+ * @name: name to print
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	while (name == NULL || f == NULL)
+		return ;
+	f(name);
+}
