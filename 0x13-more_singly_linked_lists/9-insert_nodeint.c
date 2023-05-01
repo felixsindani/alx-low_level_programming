613f6c9b4f08c1ebf5e5a9779f6d350cc4afb638
Felix Sindani <felixsindani72@gmail.com>
2023-05-01 06:08:42 -0700

    Fix output error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newNode->n = n;
 	newNode->next = NULL;
-	if (!idx)
+	if (idx == 0)
 	{
 		newNode->next = *head;
 		*head = newNode;
