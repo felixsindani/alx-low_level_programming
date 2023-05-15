580c9be488f0613f482952df6e1d967efd96ee80
Felix Sindani <felixsindani72@gmail.com>
2023-05-15 09:41:26 -0700

    Fix cp function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (old == - 1 || reading == -1)
+		if (old == -1 || reading == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(fp);
