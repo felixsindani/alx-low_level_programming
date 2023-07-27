38985d3a72a94cb02b15cb1126568b0a40dbf1df
felixsindani <felixsindani72@gmail.com>
2023-07-27 06:51:45 -0700

    spacing for elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -49,7 +49,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	printf(" magic: ");
+	printf("	magic:			");
 	for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
@@ -89,7 +89,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf("	Data:		");
+	printf("	Data:			");
 	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
@@ -112,12 +112,12 @@ void print_data(unsigned char *e_ident)
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("	version:		%d",
+	printf("	version:			%d",
 			e_ident[EI_VERSION]);
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
-			printf("	(current)\n");
+			printf("			(current)\n");
 			break;
 		default:
 			printf("\n");
@@ -131,7 +131,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf("	OS/ABI:			");
+	printf("	OS/ABI:				");
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
@@ -175,7 +175,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf("	ABI Version:		%d\n", e_ident[EI_ABIVERSION]);
+	printf("	ABI Version:			%d\n", e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -187,7 +187,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-	printf("	Type:		");
+	printf("	Type:			");
 	switch (e_type)
 	{
 		case ET_NONE:
@@ -217,7 +217,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf("	Entry point address:		");
+	printf("	Entry point address:			");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
