05a8d7918abd4aefb59781ada96184f1665cfde8
felixsindani <felixsindani72@gmail.com>
2023-08-07 12:14:33 -0700

    correct 3-cp error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,7 @@
  * Return: ptr to new buffer
  */
 char *make_buffer(char *file_name);
+void close_fd(int fd);
 
 char *make_buffer(char *file_name)
 {
@@ -38,45 +39,45 @@ void close_fd(int fd)
 
 /**
  * main - main prog
- * @ac: argument count
- * @av: arguments supplied
+ * @argc: argument count
+ * @argv: arguments supplied
  * Description:ac is incorrect- exit code 97
  * fail to close file - exit code 100
  * fail to create file - exit code 99
- * file does not exist - cose 98
+ * file does not exist - code 98
  * Return: 0 success
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 	int read_file, write_file, from, to;
 	char *buf;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage:cp file-from file_to\n");
 		exit(97);
 	}
-	buf = make_buffer(av[2]);
-	from = open(av[1], O_RDONLY);
+	buf = make_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
 	read_file = read(from, buf, 1024);
-	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || read_file == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
 		write_file = write(to, buf, read_file);
 		if (to == -1 || write_file == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
 		read_file = read(from, buf, 1024);
-		to = open(av[2], O_WRONLY | O_APPEND);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (read_file > 0);
 	free(buf);
 	close_fd(from);
